c55a1c7af0b0ec73b96771ae885ced1c741afa20
Evangelos <evangeloskolimitras@outlook.com>
2021-07-11 01:14:53 +0200

    init

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-<div style="text-align:center"><img src="./logo.png" /></div>
-
-# The Official Project M
-
-### Description
-
-##### _The project M_ is a component based website for creative photographers.
-
-### Project initialization
-
-- `yarn install`
-- `yarn start`
-
-### Bootsraped
-
-#### Create-React-App **--template Typescript**
-
-### Licence MIT
+# The Project M
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import LogoIcon from "./components/LogoIcon";
-import React from "react";
-import { useTheme } from "@material-ui/core";
+import React from 'react';
+import './App.css';
 
-interface Props {
-  title: string
+function App() {
+  return (
+    <div className="App">
+      <h1>The project m</h1>
+    </div>
+  );
 }
 
-export const App = (props: Props) => {
-  const bgColor = useTheme().palette
-  const classes = {
-    container: {
-      width: "100vw",
-      height:"100vh",
-      display: "flex",
-      justifyContent:"center",
-      alignItems: "center",
-      backgroundColor: "#ffffff", 
-      backgroundImage: `linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%)`}
-  }
- return (
-  <div style={classes.container}>
-    <LogoIcon />
-  </div>
- )
-}
+export default App;
