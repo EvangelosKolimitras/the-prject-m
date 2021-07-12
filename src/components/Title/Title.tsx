541676224fcc6e85672d778a3345cacd321312bb
Evangelos <evangeloskolimitras@outlook.com>
2021-07-12 23:38:39 +0200

    refactor(LoadingBar): remove unused component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,2 @@
-import { LoadingBar } from "../LoadingBar/LoadingBar";
 
-export function Title() {
-    return <>
-        <h1>The project M</h1>
-        <LoadingBar />
-    </>
-}
+export const Title = () => <h1>The project M</h1>
