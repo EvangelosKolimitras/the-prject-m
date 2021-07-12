47836424a7c30dd0c1776f7ad082a01ef45a1c8c
Evangelos <evangeloskolimitras@outlook.com>
2021-07-12 23:48:15 +0200

    refactor(Background): added in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { BackgroundImage } from './components/Background/Background';
 import { Title } from './components/Title/Title';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <BackgroundImage />
     </div>
   );
 }
