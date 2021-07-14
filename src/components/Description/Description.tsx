9b25408f1349f1d9310f3cbee30ef04d94521ed9
Evangelos <evangeloskolimitras@outlook.com>
2021-07-14 19:46:01 +0200

    feat(description): view added for description

---
+++ src/components/Description/Description.tsx
@@ -0,0 +1,9 @@
+export const Description = () => {
+    return <p>PROJECT M’’ isn’t just my brand-name since 2017. It represents the variety of themes that i love to photograph and the range of my artworks. Landscapes ,astrophotography , people & special events in order to tell stories of moments and happenings in our lives. At every shot i learn something about love, something about life or something about relationships. I am grateful to have entered myselft in the wolrd of photography.
+
+        Rcently, i ve team up with companies in order to promote their products and services. Therefore i am open to any business ideas.
+
+        Welcome to my photography page.
+
+        Panagiotis Martiou</p>
+}
