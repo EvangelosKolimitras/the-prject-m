2ec1d430b236a8cf9914ee4e7b76f8965ae6eb27
Evangelos <evangeloskolimitras@outlook.com>
2021-07-14 19:44:59 +0200

    feat(bio): new bio json added

---
+++ src/bio.ts
@@ -0,0 +1,56 @@
+export const info = {
+    name: 'panagiotis',
+    lastname: 'martiou',
+    title: 'professional photographer',
+    city: 'volos',
+    country: 'greece',
+    emails: {
+        'outook': {
+            email: 'martioupan@outlook.com'
+        }
+    },
+    studied: [
+        {
+            level: `bachelor's degree`,
+            major: 'business administration and management',
+            college: 'technical university of larisa',
+            city: 'larisa',
+            country: 'greece'
+        },
+        {
+            level: `certificate`,
+            major: 'business administration and management',
+            college: 'National and Kapodistrian University of',
+            city: 'athens',
+            country: 'greece'
+        },
+        {
+            level: `certificate`,
+            major: 'adobe photoshop',
+            college: 'Adobe',
+            city: undefined,
+            country: undefined
+        },
+        {
+            level: `certificate`,
+            major: 'adobe lightroom',
+            college: 'Adobe',
+            city: undefined,
+            country: undefined
+        },
+    ],
+    fields: [
+        'landscapes', 'portraits', 'intirior-Events', 'wedding', 'christening'
+    ],
+    social: {
+        facebook: 'https://www.facebook.com/ProjectM0/',
+        instagram: 'https://www.instagram.com/the_project_m/',
+        px500: 'https://500px.com/p/the_project_m?view=photos',
+        photoCrowd: 'https://www.photocrowd.com/photographer-community/126506/',
+        photoContest: 'https://www.photocontest.gr/users/19025/photos'
+    },
+    about_me: `I am Panagiotis Martiou a professional photographer based in Volos, Greece. I have a bachelor’s degree in business administration and Management and a certificate of digital marketing of National and Kapodistrian University of Athens. I also owed a certificate in Photoshop and Lightroom programs of adobe.
+    I was born and raised in Volos, a beautiful city in Greece. Always i am inclined to art , sports and nature. I love walking and exploring new places. My passion is travelling and this empowered me to deal with photography.
+    Photography came to my life as a hobby in 2017. I use this art as a way of expressing myself and bringing out the best in my subjects. I am always looking forward for new and creative ways to bring uniqueness into my photography.
+    I ll be very happy to work together . Feel free to contact me.`
+}
