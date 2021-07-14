0a44d31e76992aa1764cb88daf8386f26924a182
Evangelos <evangeloskolimitras@outlook.com>
2021-07-14 18:35:48 +0200

    feat(routes): export all imports at once

---
+++ src/components/Routes/index.tsx
@@ -0,0 +1,8 @@
+import { Christening } from './Christening/Christening';
+import { Weddings } from './Weddings/Weddings'
+import { Portraits } from './Portraits/Portraits'
+import { Events } from './Events/Events'
+import { FoodPhotography } from './FoodPhotography/FoodPhotography'
+import { InteriorPhotography } from './InteriorPhotography/InteriorPhotography'
+
+export const routes = { Christening, Weddings, Portraits, Events, FoodPhotography, InteriorPhotography };
