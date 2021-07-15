872a2cc06808c5b446a51ca495d9bce5f9fca3a8
Evangelos <evangeloskolimitras@outlook.com>
2021-07-15 19:30:29 +0200

    refactor(logo): added new logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import logo from './logo.png';
 import styles from './Logo.module.css';
 export const Logo = () => {
-    return (
-        <img className={styles.Logo} src={logo} alt="The project M logo" />
-    )
+    return <img className={styles.Logo} src={logo} alt="The project M logo" />
+
 }
