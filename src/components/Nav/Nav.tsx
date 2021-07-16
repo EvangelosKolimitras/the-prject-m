9d7689b92c0146ba9ea7fdb43c1101aa42eef2a2
Evangelos <evangeloskolimitras@outlook.com>
2021-07-16 21:38:11 +0200

    feat(navigation): first approach

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { map } from "../../utils/fp";
 import { NavItem } from "../NavItem/NavItem";
 import { createEntpoints, entpoints } from "./endpoints";
+import Styles from './Nav.module.css';
 
 const paths = createEntpoints(entpoints);
 
-export const Nav = () => <ul> {map(path => <NavItem key={path.id} path={path} />)(paths)} </ul>;
+export const Nav = () => <ul className={Styles.Nav}> {map(path => <NavItem key={path.id} path={path} />)(paths)} </ul>;
