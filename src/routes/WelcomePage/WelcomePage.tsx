617a4d7a3e675ed0927a45c79a6f965cb1360019
Evangelos <evangeloskolimitras@outlook.com>
2021-07-26 21:41:35 +0200

    refactor(logo): remove animated logo

--- src/routes/WelcomePage/WelcomePage.tsx
+++ src/routes/WelcomePage/WelcomePage.tsx
@@ -1,12 +1,10 @@
 import { Logo } from "../../components"
-import { EnterButton } from "../../components/EnterButton/EnterButton";
 import Styles from './WelcomePage.module.css';
 
 export const WelcomePage = () => {
     return <>
         <div className={Styles.Welcome}>
             <Logo />
-            <EnterButton />
         </div>
     </>
 };
