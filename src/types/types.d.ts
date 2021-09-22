75c59a7f907d77b346432dc67f54f9c283344986
Evangelos <evangeloskolimitras@outlook.com>
2021-09-22 21:24:44 +0200

    refactor(types): new namespace for Pexels added

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -37,3 +37,29 @@ declare namespace BioNS {
         email: string;
     }
 }
+
+declare namespace Pexels {
+    interface Response {
+        id: number;
+        width: number;
+        height: number;
+        url: string;
+        photographer: string;
+        photographer_url: string;
+        photographer_id: number;
+        avg_color: string;
+        src: Src;
+        liked: boolean;
+    }
+
+    interface Src {
+        original: string;
+        large2x: string;
+        large: string;
+        medium: string;
+        small: string;
+        portrait: string;
+        landscape: string;
+        tiny: string;
+    }
+}
